ee425b0e315fe488f439b1fa4aee87c3cb70c736
896q <896q@fresnoU.org>
2023-03-14 13:56:11 -0700

    Initial commit

---
+++ FunctionNotes.js
@@ -0,0 +1,46 @@
+//Function Notes
+//Step1 - Define the function
+function sayHello(){
+console.log("Hello");
+}
+    //Step2 - Call (use) the function
+
+ sayHello();
+
+//function printMax(){
+   // let x = 6;
+   // let y = 29;
+   // if (x>y) {
+      //  console.log(`the larger number is ${x}`);
+    //}else{
+    //    console.log(`the larger number is ${y}`);
+  //  }
+//}
+
+
+//print();
+
+//function printMax (x,y) {
+  //  if (x > y) {
+    //    console.log(`the larger number is ${x}`);
+    //} else {
+      //  console.log(`the larger number is ${y}`);
+    //}
+//}
+
+//printMax(8, 21);
+//printMax(8, 21);
+//printMax(8, 21);
+//printMax(8, 21);
+
+function sum(number1, number2){
+    console.log(`${number1} + ${number2} = ${number1+number2}`);
+}
+
+sum( 7, 12);
+sum( 6 ,5);
+
+
+
+
+
